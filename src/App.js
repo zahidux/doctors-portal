5d968634aac2911b1cb8cd328e70735b974607c7
zahidux <zahidux@gmail.com>
2022-04-17 17:48:21 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Switch>
+          <Route path="/about">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
